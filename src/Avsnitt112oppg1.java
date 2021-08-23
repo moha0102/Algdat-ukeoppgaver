c07af8f332832b6da59d949f07c9e28160636cf0
Moham <mohammedak0102@gmail.com>
2021-08-23 09:17:17 +0200

    Ukeoppgave 1.1.2 Oppgave 2)

--- src/Avsnitt112oppg1.java
+++ src/Avsnitt112oppg1.java
@@ -2,14 +2,12 @@ public class Avsnitt112oppg1 {
     public static void main(String[] args) {
         int[] values = {8,4,17,10,6,20,1,11,15,3,18,9,2,7,19};
         min(values);
-
         int minValue = min(values);
         System.out.println(minValue);
 
     }
 
     static int min(int[] values) {
-
         int minValue = values[0];
         int pos = 0;
 
